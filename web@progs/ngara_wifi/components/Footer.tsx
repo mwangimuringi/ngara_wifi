e8276b97625c4327b449184e6e26da6d4c4bd372
julius_mwangi <mwangimuringi10@gmail.com>
2024-06-16 20:57:15 +0300

    Added the footer

--- web@progs/ngara_wifi/components/Footer.tsx
+++ web@progs/ngara_wifi/components/Footer.tsx
@@ -59,7 +59,7 @@ const Footer = () => {
         </div>
 
         <div className="border bg-gray-20" />
-        <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+        <p className="regular-14 w-full text-center text-gray-30">2024 Ngara Wifi | All rights reserved</p>
       </div>
     </footer>
   )
